efa1a880400ad9ec5f64ca7cb17ea1f89ab7c216
Ruthwik <ruthwik.masina@gmail.com>
2019-09-21 15:26:09 +0530

    created services using docker-compose

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # Schema-Registry-with-Avro-Schema
 
+Pre Requesites
+Docker installed
+Local Registry up and running on port 5000
 
 Steps
-
-1. Run Kafka
-2. Create Topics avro.topic1,avro.topic2
-3. Run Schema Registry and landoop docker run --rm -p 8000:8000 -e "SCHEMAREGISTRY_URL=http://localhost:8081" -e "PROXY=true" landoop/schema-registry-ui
-4. mvn clean install - will create a docker image in local Registry
-5. docker network create local_network
-6. docker-compose to run the services
+1. mvn clean install - will create a docker image in local Registry (both the services)
+2. docker network create local_network
+3. docker-compose to run the services
+  This will run
+   a. zookeeper
+   b. kafka
+   c. schema registry
+   d. landoop: A GUI for schema registry http://localhost:8000/#/
+   e. Admission Manager service http://localhost:7070/admission/v1/swagger-ui.html
+   f. Student Manager Service http://localhost:9090/student/v1/swagger-ui.html
--- student-manager-service/src/main/java/com/student/manager/kafka/config/SchemRegistryConfig.java
+++ student-manager-service/src/main/java/com/student/manager/kafka/config/SchemRegistryConfig.java
@@ -1,18 +1,24 @@
 package com.student.manager.kafka.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.schema.client.ConfluentSchemaRegistryClient;
 import org.springframework.cloud.stream.schema.client.SchemaRegistryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class SchemRegistryConfig {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(SchemRegistryConfig.class);
+	
     @Bean
     public SchemaRegistryClient schemaRegistryClient(@Value("${spring.cloud.stream.kafka.binder.producer-properties.schema.registry.url}") String endPoint) {
+    	
+    	LOGGER.info("schemaRegistryClient() :: Creating ConfluentSchemaRegistryClient");
         ConfluentSchemaRegistryClient client = new ConfluentSchemaRegistryClient();
-        System.out.println("schemaRegistryClient");
         client.setEndpoint(endPoint);
         return client;
     }
